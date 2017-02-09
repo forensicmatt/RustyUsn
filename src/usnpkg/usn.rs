f70f3999315aac1ee8225db43e27375b5f2cc5d1
matthew seyer <matthew.seyer@gmail.com>
2017-02-08 23:02:57 -0600

    moved majority of read operations to read_record method; Added new structs and custom errors; added verbose;

--- src/usnpkg/usn.rs
+++ src/usnpkg/usn.rs
@@ -1,4 +1,3 @@
-use usnpkg::chrono::*;                                  //Datetime Handling
 use usnpkg::byteorder::{ReadBytesExt, LittleEndian};    //Reading little endian data structs
 use std::fs::File;                                      //File handle
 use std::io::{Error, ErrorKind};                        //for error handling
@@ -7,49 +6,55 @@ use std::io::Seek;                                      //for Seeking our File
 use std::io::SeekFrom;                                  //for Seeking our File
 use std::slice;                                         //for going from binary to structures
 use std::mem;                                           //for initializing our USN struct
+use usnpkg::usn_errors;
+use usnpkg::timestamp::{WinTimestamp};
 
-#[derive(Debug)] //So we can print our structure
 // Structure reference:
 // https://msdn.microsoft.com/en-us/library/windows/desktop/aa365722(v=vs.85).aspx
+#[derive(Debug)]
 pub struct UsnRecordV2 {
     // 0
-    record_length: u32,
-    major_version: u16,
-    minor_version: u16,
-    file_reference_number: u64,
-    parent_file_reference_number: u64,
-    usn: u64,
-    // 32
-    timestamp: NaiveDateTime, // Holds our datetime
-    // 40
-    reason: u32,
-    source_info: u32,
-    security_id: u32,
-    file_attributes: u32,
-    file_name_length: u16,
-    file_name_offset: u16,
+    pub record_length: u32,
+    pub major_version: u16,
+    pub minor_version: u16,
+    pub file_reference_number: u64,
+    pub parent_file_reference_number: u64,
+    pub usn: u64,
+    pub timestamp: WinTimestamp,
+    pub reason: u32,
+    pub source_info: u32,
+    pub security_id: u32,
+    pub file_attributes: u32,
+    pub file_name_length: u16,
+    pub file_name_offset: u16,
     // 60
-    file_name: String // For unicode
+    pub file_name: String // For unicode
 }
 
 // maintain UsnConnection info
 pub struct UsnConnection {
     filehandle: File, // The filehandle
+    _verbose: bool,   // verbose output
     _offset: u64,     // maintain where we are in the file
     _size: u64        // the size of the file
 }
 
+pub struct UsnResult (
+    pub UsnRecordV2, // The record
+    pub u64          // The offset
+);
+
 // implement UsnConnection Functionality
-impl UsnConnection{
+impl UsnConnection {
     // function for getting a record
-    pub fn get_next_record(&mut self)->Result<UsnRecordV2,Error>{
+    pub fn get_next_record(&mut self)->Result<UsnResult,Error>{
         loop {
             // Check that our offset is not past the end of file
-            if self._offset >= self._size{
-                return Err(Error::new(ErrorKind::Other, "End of File"))
+            if self._offset >= self._size {
+                return Err(Error::new(ErrorKind::Other, "End of File"));
             }
 
-            println!("function: get_next_record() at offset: {}", self._offset);
+            // println!("function: get_next_record() at offset: {}", self._offset);
 
             // Seek to offset
             let soffset = match self.filehandle.seek(SeekFrom::Start(self._offset)){
@@ -57,87 +62,30 @@ impl UsnConnection{
                 Err(error) => return Err(error)
             };
 
-            if soffset > self._size{
-                return Err(Error::new(ErrorKind::Other, "End of File"))
+            if soffset > self._size {
+                return Err(Error::new(ErrorKind::Other, "End of File"));
             }
 
-            // init record struct
-            let mut record: UsnRecordV2 = unsafe {
-                mem::zeroed()
+            let record: UsnRecordV2 = match read_record(self.filehandle.try_clone().unwrap()) {
+                Ok(record) => record,
+                Err(error) => {
+                    if self._verbose {
+                        println!("{:?}",error);
+                    }
+                    self._offset = self._offset + 8;
+                    continue;
+                }
             };
 
-            ///////////////////////
-            // Read structure
-            ///////////////////////
-            record.record_length = self.filehandle.read_u32::<LittleEndian>().unwrap();
+            self._offset = self._offset + (record.record_length as u64);
 
-            // Do some record checks first
-            if record.record_length == 0{
-                self._offset += 8;
-                continue;
-            }
-            // TODO: Add additional checks here
-
-            // Parse next 28 bytes
-            record.major_version = self.filehandle.read_u16::<LittleEndian>().unwrap();
-            record.minor_version = self.filehandle.read_u16::<LittleEndian>().unwrap();
-            record.file_reference_number = self.filehandle.read_u64::<LittleEndian>().unwrap();
-            record.parent_file_reference_number = self.filehandle.read_u64::<LittleEndian>().unwrap();
-            record.usn = self.filehandle.read_u64::<LittleEndian>().unwrap();
-
-            // Create datetime epoch (Windows epoch is 1601-01-01)
-            record.timestamp = NaiveDate::from_ymd(1601, 1, 1).and_hms_nano(0, 0, 0, 0);
-            // Get nanoseconds (100-nanosecond intervals)
-            let t_nano = self.filehandle.read_i64::<LittleEndian>().unwrap();
-            // Convert to microseconds
-            let t_micro = t_nano / 10;
-            // Add microseconds to timestamp via Duration
-            record.timestamp = record.timestamp + duration::Duration::microseconds(t_micro);
-
-            // Parse next 20 bytes
-            record.reason = self.filehandle.read_u32::<LittleEndian>().unwrap();
-            record.source_info = self.filehandle.read_u32::<LittleEndian>().unwrap();
-            record.security_id = self.filehandle.read_u32::<LittleEndian>().unwrap();
-            record.file_attributes = self.filehandle.read_u32::<LittleEndian>().unwrap();
-            record.file_name_length = self.filehandle.read_u16::<LittleEndian>().unwrap();
-            record.file_name_offset = self.filehandle.read_u16::<LittleEndian>().unwrap();
-
-            // Create a vector to store the byte buffer
-            let mut buff_name = Vec::<u8>::with_capacity((record.file_name_length) as usize);
-            unsafe {
-                // set size of byte buffer
-                buff_name.set_len(record.file_name_length as usize);
-            }
-
-            // read into byte buffer
-            match self.filehandle.read(&mut buff_name[..]){
-                Ok(bytes_read) => bytes_read,
-                Err(error) => return Err(error)
-            };
-
-            // create a utf-16 buffer from the byte buffer
-            let wchar_buff: &[u16] = unsafe {
-                // slice into 2 byte pieces
-                slice::from_raw_parts(
-                    buff_name.as_ptr() as *const u16,
-                    buff_name.len() / 2
-                )
-            };
-
-            // set record file_name
-            record.file_name = String::from_utf16(wchar_buff).unwrap();
-
-            // set new offset
-            self._offset += record.record_length as u64;
-
-            // return record
-            return Ok(record);
+            return Ok(UsnResult(record,soffset as u64));
         }
     }
 }
 
 // Get UsnConnection from filename
-pub fn open_file(filename: &str)->UsnConnection{
+pub fn open_file(filename: &str, verbose: bool)->UsnConnection{
     // Open a filehandle to the file
     let mut usn_fh = match File::open(filename) {
         Ok(usn_fh) => usn_fh,
@@ -154,6 +102,7 @@ pub fn open_file(filename: &str)->UsnConnection{
     // Create UsnConnection
     let usn_connection = UsnConnection{
         filehandle: usn_fh,
+        _verbose: verbose,
         _offset: 0,
         _size: size
     };
@@ -163,15 +112,105 @@ pub fn open_file(filename: &str)->UsnConnection{
 }
 
 // Get Record from Buffer
-pub fn parse_record(buffer: &[u8])->Result<UsnRecordV2,Error>{
-    // Create record and initialize it
+pub fn read_record<R: Read>(mut buffer: R)->Result<UsnRecordV2,usn_errors::UsnError>{
+    // init record struct
     let mut record: UsnRecordV2 = unsafe {
         mem::zeroed()
     };
 
-    // record.record_length = unsafe {
-    //     mem::transmute(buffer[4])
-    // };
+    record.record_length = buffer.read_u32::<LittleEndian>()?;
+
+    // Do some record checks first
+    if record.record_length == 0 {
+        return Err(
+            usn_errors::UsnError::invalid_v2_record(
+                format!("Record length is 0.")
+            )
+        );
+    }
+    if record.record_length > 1024 {
+        return Err(
+            usn_errors::UsnError::invalid_v2_record(
+                format!("Record length is over 1024.")
+            )
+        );
+    }
+
+    record.major_version = buffer.read_u16::<LittleEndian>()?;
+    if record.major_version != 2 {
+        return Err(
+            usn_errors::UsnError::invalid_v2_record(
+                format!("Major version is not 2")
+            )
+        );
+    }
+    record.minor_version = buffer.read_u16::<LittleEndian>()?;
+    if record.minor_version != 0 {
+        return Err(
+            usn_errors::UsnError::invalid_v2_record(
+                format!("Minor version is not 0")
+            )
+        );
+    }
+    record.file_reference_number = buffer.read_u64::<LittleEndian>()?;
+    record.parent_file_reference_number = buffer.read_u64::<LittleEndian>()?;
+    record.usn = buffer.read_u64::<LittleEndian>()?;
+    record.timestamp = WinTimestamp(buffer.read_u64::<LittleEndian>()?);
+    record.reason = buffer.read_u32::<LittleEndian>()?;
+    record.source_info = buffer.read_u32::<LittleEndian>()?;
+    record.security_id = buffer.read_u32::<LittleEndian>()?;
+    record.file_attributes = buffer.read_u32::<LittleEndian>()?;
+    record.file_name_length = buffer.read_u16::<LittleEndian>()?;
+    record.file_name_offset = buffer.read_u16::<LittleEndian>()?;
+
+    if record.file_name_offset != 60 {
+        return Err(
+            usn_errors::UsnError::invalid_v2_record(
+                format!("file_name_offset is not 60")
+            )
+        );
+    }
+
+    // Create a vector to store the byte buffer
+    let mut buff_name = vec![0u8; record.file_name_length as usize];
+
+    // read into byte buffer
+    let bytes_read = match buffer.read(&mut buff_name[..]){
+        Ok(bytes_read) => bytes_read,
+        Err(error) => return Err(
+            usn_errors::UsnError::io_error(
+                format!("IO Error: {:?}",error)
+            )
+        )
+    };
+
+    if bytes_read < record.file_name_length as usize {
+        return Err(
+            usn_errors::UsnError::io_error(
+                format!("Not enough bytes for filename buffer.")
+            )
+        )
+    }
+
+    // create a utf-16 buffer from the byte buffer
+    let wchar_buff: &[u16] = unsafe {
+        // slice into 2 byte pieces
+        slice::from_raw_parts(
+            buff_name.as_ptr() as *const u16,
+            buff_name.len() / 2
+        )
+    };
+
+    // set record file_name
+    record.file_name = match String::from_utf16(wchar_buff) {
+        Ok(file_name) => file_name,
+        Err(error) => return Err(
+            usn_errors::UsnError::io_error(
+                // Better Error needed here - Not really IO Error
+                format!("IO Error: {:?}",error)
+            )
+        )
+    };
 
     Ok(record)
 }
