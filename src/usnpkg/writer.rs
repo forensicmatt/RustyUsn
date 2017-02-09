ab69a6e96628f4e5b798186e94207b9df2d68b3f
matthew seyer <matthew.seyer@gmail.com>
2017-02-08 22:55:41 -0600

    writer module to handle record output operations

---
+++ src/usnpkg/writer.rs
@@ -0,0 +1,51 @@
+use usnpkg::usn;
+
+pub struct Writer {}
+
+impl Writer {
+    pub fn new() -> Writer {
+        Writer {}
+    }
+
+    pub fn write_header(&mut self) {
+        println!(
+            "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
+            "offset",
+            "record_length",
+            "major_version",
+            "minor_version",
+            "file_reference_number",
+            "parent_file_reference_number",
+            "usn",
+            "timestamp",
+            "reason",
+            "source_info",
+            "security_id",
+            "file_attributes",
+            "file_name_length",
+            "file_name_offset",
+            "file_name"
+        );
+    }
+
+    pub fn write_record(&mut self, record: usn::UsnRecordV2, offset: u64) {
+        println!(
+            "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
+            offset,
+            record.record_length,
+            record.major_version,
+            record.minor_version,
+            record.file_reference_number,
+            record.parent_file_reference_number,
+            record.usn,
+            record.timestamp,
+            record.reason,
+            record.source_info,
+            record.security_id,
+            record.file_attributes,
+            record.file_name_length,
+            record.file_name_offset,
+            record.file_name
+        );
+    }
+}
