eceade1b32c2fe1486f4199d67734a8afe96c3b2
matthew seyer <matthew.seyer@gmail.com>
2017-02-08 23:01:05 -0600

    commenting out for now...

--- src/python/pyusn.rs
+++ src/python/pyusn.rs
@@ -1,26 +1,26 @@
-use cpython::{PyObject, PyResult, Python, PyTuple, PyDict};
-use usnpkg::usn;
-
-py_class!(class PyUsnConnection |py| {
-    def __new__(_cls) -> PyResult<PyUsnConnection> {
-        PyUsnConnection::create_instance(py)
-    }
-});
-
-// add bindings to the generated python module
-py_module_initializer!(rustyusn, initrustyusn, PyInit_rustyusn, |py, m| {
-    m.add(py, "__doc__", "This module is implemented in Rust.")?;
-    m.add(py, "open_file", py_fn!(py, open_file_py(filename: &str)))?;
-    Ok(())
-});
-
-fn open_file_py(py: Python,filename: &str) -> PyResult<PyObject> {
-    println!("Opening {}",filename);
-
-    // get UsnConnection from a filename
-    let mut usn_connection = usn::open_file(
-        filename
-    );
-
-    Ok(py.None())
-}
+// use cpython::{PyObject, PyResult, Python, PyTuple, PyDict};
+// use usnpkg::usn;
+//
+// py_class!(class PyUsnConnection |py| {
+//     def __new__(_cls) -> PyResult<PyUsnConnection> {
+//         PyUsnConnection::create_instance(py)
+//     }
+// });
+//
+// // add bindings to the generated python module
+// py_module_initializer!(rustyusn, initrustyusn, PyInit_rustyusn, |py, m| {
+//     m.add(py, "__doc__", "This module is implemented in Rust.")?;
+//     m.add(py, "open_file", py_fn!(py, open_file_py(filename: &str)))?;
+//     Ok(())
+// });
+//
+// fn open_file_py(py: Python,filename: &str) -> PyResult<PyObject> {
+//     println!("Opening {}",filename);
+//
+//     // get UsnConnection from a filename
+//     let mut usn_connection = usn::open_file(
+//         filename
+//     );
+//
+//     Ok(py.None())
+// }
