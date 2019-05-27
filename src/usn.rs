9438cf31308da0918c654c1172f7c90d6a70c98c
Matthew Seyer <matthew.seyer@gmail.com>
2019-05-27 10:08:17 -0600

    added search size
    separated chunk size and search size;

--- src/usn.rs
+++ src/usn.rs
@@ -5,6 +5,14 @@ use std::io::SeekFrom;
 use crate::ReadSeek;
 use crate::record::UsnEntry;
 
+// This is the size of data chunks
+const SIZE_CHUNK: usize = 5120;
+// This is the size of the search within a chunk
+// It is smaller than the chunk size to garentee that the last record found is complete
+// It has been noticed that generally usn records are paged in 4096 byte pages. I have not
+// observed usn records overlaping the 4096 offset and are zero padded to the 4096 mark.
+const SIZE_SEARCH: usize = 4096;
+
 
 pub struct UsnParser<T: ReadSeek> {
     _inner_handle: T,
@@ -38,26 +46,46 @@ impl <T: ReadSeek> UsnParser <T> {
     pub fn get_chunk_iterator(&mut self) -> IterFileChunks<T> {
         IterFileChunks{
             parser: self,
-            chunk_size: 4096,
-            current_offset: 0,
+            chunk_size: SIZE_CHUNK,
+            search_size: SIZE_SEARCH,
+            chunk_start_offset: 0,
         }
     }
 }
 
 pub struct IterFileChunks<'c, T: ReadSeek> {
     parser: &'c mut UsnParser<T>,
+    // The chunk size is larger than the parse size to ensure complete end record
     chunk_size: usize,
-    current_offset: u64,
+    // The parse size is smaller than the chunk size to ensure there is overlap for complete end record
+    search_size: usize,
+    // This is the relative offset of this chunk
+    chunk_start_offset: u64,
 }
 
 impl <'c, T: ReadSeek> Iterator for IterFileChunks <'c, T> {
     type Item = DataChunk;
 
     fn next(&mut self) -> Option<<Self as Iterator>::Item> {
-        while self.current_offset < self.parser._size {
-            let current_offset = self.current_offset;
+        while self.chunk_start_offset < self.parser._size {
+            // Create buffer for our data chunk
             let mut buffer = vec![0u8; self.chunk_size];
 
+            // Get the current offset
+            let current_offset = self.chunk_start_offset;
+            
+            // Seek to where we start our chunk
+            match self.parser._inner_handle.seek(
+                SeekFrom::Start(current_offset)
+            ) {
+                Ok(_) => {},
+                Err(error) => {
+                    error!("{}", error);
+                    break;
+                }
+            }
+
+            // Read into buffer
             let _bytes_read = match self.parser._inner_handle.read(
                 buffer.as_mut_slice()
             ){
@@ -68,11 +96,15 @@ impl <'c, T: ReadSeek> Iterator for IterFileChunks <'c, T> {
                 }
             };
 
-            self.current_offset += _bytes_read as u64;
+            // Set the next chunk's offset
+            // Increment by search size and not chunk size
+            self.chunk_start_offset += self.search_size as u64;
 
+            // Return data chunk
             return Some(
                 DataChunk{
                     offset: current_offset,
+                    search_size: self.search_size,
                     data: buffer
                 }
             );
@@ -85,6 +117,7 @@ impl <'c, T: ReadSeek> Iterator for IterFileChunks <'c, T> {
 #[derive(Debug)]
 pub struct DataChunk {
     offset: u64,
+    search_size: usize,
     data: Vec<u8>
 }
 
@@ -93,7 +126,8 @@ impl DataChunk {
         debug!("Getting DataChunk at offset: {}", self.offset);
         let record_iterator = IterRecords::new(
             self.data, 
-            self.offset
+            self.offset,
+            self.search_size
         );
 
         let records: Vec<UsnEntry> = record_iterator.collect();
@@ -109,11 +143,11 @@ pub struct IterRecords {
 }
 
 impl IterRecords {
-    pub fn new(block: Vec<u8>, start_offset: u64) -> IterRecords {
+    pub fn new(block: Vec<u8>, start_offset: u64, search_size: usize) -> IterRecords {
         let usn_regex = bytes::Regex::new("..\x00\x00\x02\x00\x00\x00").unwrap();
 
         let mut match_offsets: Vec<u64> = Vec::new();
-        for hit in usn_regex.find_iter(&block) {
+        for hit in usn_regex.find_iter(&block[..search_size]) {
             match_offsets.push(hit.start() as u64);
         }
 
